23c9f65b074a880e29e14ac7bb09e87220f961e2
Andra <andra.popa1989@gmail.com>
2018-08-28 06:38:58 +0100

    font size h3 changed

--- script.js
+++ script.js
@@ -2,11 +2,13 @@ var h3 = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.querySelector("#body");
-
+h3.style.fontFamily = "Roboto";
 
 function gradient() {
+    
     body.style.background = "linear-gradient(to right bottom, " + color1.value + ", " + color2.value + ")";
     h3.textContent = body.style.background + ";";
+    
 }
 
 color1.addEventListener("input", gradient);
